5fccc189cc9560382fdf71de2121c477111263a5
Swapnil Sahare <swapnilsahare45@gmail.com>
2025-11-26 19:40:10 +0530

    change app response type based on success

--- types/response.ts
+++ types/response.ts
@@ -1,9 +1,5 @@
 
 // Type for all responses across the application
-export type AppResponse<T = undefined> = {
-    success: boolean;
-    message?: string;
-    data?: T;
-    error?: string,
-    errors?: Record<string, string[]>;
-};
+export type AppResponse<T = unknown> =
+    | { success: true; data?: T; message?: string }
+    | { success: false; error?: string; errors?: Record<string, string[]> };
